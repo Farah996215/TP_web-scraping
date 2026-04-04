0d655a899f81482743c76753df159f7b7ca199b0
naadaa22 <farahmzoughi11@gmail.com>
2026-04-04 18:06:30 +0100

    Extract job posts by selecting top-level comments from parsed HTML

--- scraper.py
+++ scraper.py
@@ -2,15 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 
 def main():
-  url = "https://news.ycombinator.com/item?id=42919502"
-  response = requests.get(url)
+    url = "https://news.ycombinator.com/item?id=42919502"
+    response = requests.get(url)
 
-  soup = BeautifulSoup(response.content, "html.parser")
-  # find all elements with class="comment"
-  elements = soup.find_all(class_="comment")
+    soup = BeautifulSoup(response.content, "html.parser")
 
-  # Show the number of elementd found
-  print(f"Elements: {len(elements)}")
+    # find all elements with class="ind" and indent level = 0
+    elements = soup.find_all("td", class_="ind", style="margin-left:0px")
+
+    # for each of this elements, find the next element
+    comments = [e.find_next(class_="comment") for e in elements]
+
+    print(f"Comments: {len(comments)}")
+
+    # show each comment (job post)
+    for comment in comments:
+        print(comment.text)
+        print("-" * 80)
 
 if __name__ == "__main__":
-  main()
+    main()
